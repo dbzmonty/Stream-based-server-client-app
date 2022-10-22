b57fa117e214d7edab6a93240e656202a533ca4f
Dobozi Andras <dbzmonty@gmail.com>
2022-10-22 13:36:08 +0200

    One time communication between server and client

--- Stream-based-server-client-app/Client-app/Program.cs
+++ Stream-based-server-client-app/Client-app/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Net.Sockets;
+using System.IO;
 
 namespace Client_app
 {
@@ -10,7 +14,32 @@ namespace Client_app
     {
         static void Main(string[] args)
         {
+            string serverIpAddress = ConfigurationManager.AppSettings["ServerIpAddress"];
+            string ServerPort = ConfigurationManager.AppSettings["ServerPort"];
+            IPAddress ip = IPAddress.Parse(serverIpAddress);
+            int port = int.Parse(ServerPort);
 
+            try
+            {
+                TcpClient client = new TcpClient();
+                client.Connect(ip, port);
+
+                StreamReader r = new StreamReader(client.GetStream(), Encoding.UTF8);
+                string info = r.ReadLine();
+                Console.WriteLine(info);
+
+                StreamWriter w = new StreamWriter(client.GetStream(), Encoding.UTF8);
+                w.WriteLine("Hello, this is a client!");
+                w.Flush();
+
+                Console.WriteLine("Client quit!");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
         }
     }
 }
--- Stream-based-server-client-app/Server-app/Program.cs
+++ Stream-based-server-client-app/Server-app/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 
 namespace Server_app
 {
@@ -10,6 +14,41 @@ namespace Server_app
     {
         static void Main(string[] args)
         {
+            string serverIpAddress = ConfigurationManager.AppSettings["ServerIpAddress"];
+            string ServerPort = ConfigurationManager.AppSettings["ServerPort"];
+            IPAddress ip = IPAddress.Parse(serverIpAddress);
+            int port = int.Parse(ServerPort);
+
+            try
+            {
+                TcpListener listener = new TcpListener(ip, port);
+                listener.Start();
+
+                Console.WriteLine("Server online, waiting for connection!");
+                TcpClient client = listener.AcceptTcpClient();
+
+                string connectedClient = client.Client.RemoteEndPoint.ToString();
+                Console.WriteLine("Client connected: {0}", connectedClient);
+
+                StreamWriter w = new StreamWriter(client.GetStream(), Encoding.UTF8);
+                w.WriteLine("Stream Server v1.0");
+                w.Flush();
+
+                StreamReader r = new StreamReader(client.GetStream(), Encoding.UTF8);
+                string incoming = r.ReadLine();
+                Console.WriteLine(incoming);
+
+                Console.WriteLine("Server quit!");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+
+
+
 
         }
     }
